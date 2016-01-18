81c86acf4c1d44ee1f08afa99bacbed35207a132
diamant1991 <diamant1988@mail.ru>
2016-01-18 17:10:52 +0200

    add other files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+$(':input').blur(function() {
+  if($(this).val().length == 0) {
+    $(this)
+      .addClass('error')
+      $(this).parent().append('<span class="tolltip">Неправильно заполнено поле.</span>')
+      $(this).parent().find('.tolltip').animate({opacity: '1', marginTop: '0'}, 350)
+  }
+});
+$(':input').focus(function() {
+  $(this)
+    .removeClass('error')
+    $(this).parent().find('.tolltip').removeClass('active')
+    $(this).parent().find('.tolltip').animate({opacity: '0', marginTop: '15px'}, 350).remove()
+});
+
+$('form').bind('submit', function(event) {
+  $(this).find('[type=text], textarea').each(function() {
+    if(!$(this).val().length) { 
+    event.preventDefault();
+      $(this).addClass('error');
+      $(this).parent().append('<span class="tolltip">Неправильно заполнено поле.</span>')
+      $(this).parent().find('.tolltip').animate({opacity: '1', marginTop: '0'}, 350)
+    }
+  });
+});
+
+
+
 $(document).ready(function() {
 	$('.enroll-toggle').click(function(e) {
 		e.preventDefault();
@@ -14,4 +42,19 @@ $(document).ready(function() {
 			popup.removeClass('active');
 		};
 	});
+
+	$('.ques-toggle').click(function(e) {
+		e.preventDefault();
+		var text = $(this).next();
+		$('.ques-text').slideUp(300);
+		$('.ques-toggle').removeClass('active');
+		if(text.is(':hidden')){
+			$(this).addClass('active');
+			text.slideDown(300);
+		}
+		else{
+			text.slideUp(300);
+		}
+	});
 });
+
